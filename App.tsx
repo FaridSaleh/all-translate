58f1b3c977c5a18fe2f29991f1c07b53b5b7c115
FaridSaleh <faridsaleh1369@gmail.com>
2025-09-29 16:45:36 +0330

    feat: add tailwind config for colors + add gradient library

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import { NewAppScreen } from '@react-native/new-app-screen'
-import { StatusBar, useColorScheme, View } from 'react-native'
-import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context'
+import { StatusBar, Text, useColorScheme, View } from 'react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import LinearGradient from 'react-native-linear-gradient'
 import './global.css'
 
 function App() {
@@ -15,12 +15,19 @@ function App() {
 }
 
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets()
-
   return (
-    <View className="flex-1 items-center justify-center">
-      <NewAppScreen templateFileName="App.tsx" safeAreaInsets={safeAreaInsets} />
-    </View>
+    <LinearGradient
+      colors={['#D2E5FF', '#F4F8FF', '#FFFFFF']}
+      locations={[0, 0.6, 1]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 0, y: 1 }}
+    >
+      <View className="w-screen h-screen">
+        <View className="flex-1 items-center justify-center">
+          <Text className="text-[32px] font-bold text-text-primary">All Translate</Text>
+        </View>
+      </View>
+    </LinearGradient>
   )
 }
 
