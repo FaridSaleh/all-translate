f98a3ccb231894cc2387586c1ef239ce7eaa3f8a
FaridSaleh <faridsaleh1369@gmail.com>
2025-10-01 13:32:18 +0330

    fix: fix crash issue on android by upgrading react-native-safe-area-context

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import { StatusBar, useColorScheme } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import AppNavigator from '@/screens'
 import '@/i18n'
-import 'react-native-gesture-handler'
 import './global.css'
 
 function App() {
