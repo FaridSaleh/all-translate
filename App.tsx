c0d732bf86e1f03c1013743a7e1151df382815e2
FaridSaleh <faridsaleh1369@gmail.com>
2025-10-08 12:32:48 +0330

    feat: add SF Pro font

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import { StatusBar, useColorScheme } from 'react-native'
+import { StatusBar, useColorScheme, LogBox } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { QueryClientProvider } from '@tanstack/react-query'
 import AppNavigator from '@/screens'
 import '@/i18n'
 import './global.css'
 import queryClient from '@/utils/api/queryClient'
+
+LogBox.ignoreAllLogs(true)
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark'
